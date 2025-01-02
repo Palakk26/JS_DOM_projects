deae7ee946d333dd7bee5fc4abad21c41a886297
Palakk26 <palakraghuwanshi2692321@gmail.com>
2025-01-02 09:05:43 +0530

    Add files via upload

---
+++ Swap(7)/script.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector('button')
+let img1 = document.querySelector('#img1')
+let img2 = document.querySelector('#img2')
+
+btn.addEventListener('click',function(){
+    let img1src = img1.getAttribute('src')
+    let img2src = img2.getAttribute('src')
+    img1.setAttribute('src',img2src)
+    img2.setAttribute('src',img1src)
+})
